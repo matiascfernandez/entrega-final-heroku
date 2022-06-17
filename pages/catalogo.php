385bc4c855d1f271dbd4e40fca2cb0698c800e68
Matías Fernández <matiascffernandez@gmail.com>
2022-06-17 17:46:09 -0300

    e

--- pages/catalogo.php
+++ pages/catalogo.php
@@ -55,15 +55,15 @@
                 <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne"
                     data-bs-parent="#accordionExample">
                     <div class="accordion-body">
-                        <div class= "imagenes__divc"><img class="imagenes transition:0.6s" width="330" src="https://img.freepik.com/free-photo/portrait-happy-smiling-young-businessman-blue-suit-isolated-white-wall_231208-9205.jpg?w=360&t=st=1650476027~exp=1650476627~hmac=daab808c413f9dfa2bc9f898a243a2e91ad99185ed003ffaac0f6722c5ec62f2" alt=""></div>
-                        <div class= "imagenes__divc"><img class="imagenes transition:0.6s" width="330" src="https://img.freepik.com/free-photo/happy-business-man-wearing-grey-suit-standing-isolated-white-wall_231208-9211.jpg?w=360&t=st=1650475927~exp=1650476527~hmac=29590b8111508c31aebb2507a86d69e194bfb0c3c234836704a6c88a2048a5b2" alt=""></div>
-                        <div class= "imagenes__divc"><img class="imagenes transition:0.6s" width="330" src="https://img.freepik.com/free-photo/portrait-happy-smiling-young-businessman-brown-suit-isolated-white-wall_231208-9214.jpg?w=360&t=st=1650475876~exp=1650476476~hmac=658088d702884f8b4ad12cdfd79562f675bad3d68a24af6dbd855167693d3d6c" alt=""></div>
-                        <div class= "imagenes__divc"><img class="imagenes transition:0.6s" width="330" src="https://img.freepik.com/free-photo/full-body-portrait-businessman-suit-standing-isolated-white-wall_231208-9203.jpg?t=st=1650477958~exp=1650478558~hmac=dcb63693b7d8f006de49abe5cc686102b8e1461dbcdac824e7b6ae05855a1c92&w=360" alt=""></div>
-                        <div class= "imagenes__divc"><img class="imagenes transition:0.6s" width="330" src="https://img.freepik.com/free-photo/full-length-portrait-businessman-isolated-gray-wall_231208-9209.jpg?t=st=1650477958~exp=1650478558~hmac=feccece7be6cf7b73e4f3943a0b1637fac1fe5818cf604ff8912b01591d0e69c&w=360" alt=""></div>
-                        <div class= "imagenes__divc"><img class="imagenes transition:0.6s" width="330" src="https://img.freepik.com/free-photo/portrait-happy-smiling-young-businessman-brown-suit-isolated-white-wall_231208-9213.jpg?w=360&t=st=1650477966~exp=1650478566~hmac=aa9ff0b6f74c7d9d128c8eb999163648266b2b2ae3255b0f57129b3f5e704e13" alt=""></div>
-                        <div class= "imagenes__divc"><img class="imagenes transition:0.6s" width="330" src="https://img.freepik.com/foto-gratis/retrato-cuerpo-entero-empresario-blanco_155003-16199.jpg?w=360&t=st=1654194236~exp=1654194836~hmac=4396ecbf619be12b35c6acdcf17170d55cab2e60d3627fe890bd12396321961f" alt=""></div>
-                        <div class= "imagenes__divc"><img class="imagenes transition:0.6s" width="330" src="https://img.freepik.com/foto-gratis/concepto-negocio-retrato-cuerpo-entero-hombre-negocios-elegante-traje-elegante-caminando-sobre-fondo-blanco_1258-80289.jpg?t=st=1654188714~exp=1654189314~hmac=15550dfa18d069d979f6b59a751711d89c52ee645d7e2974a0893690e5d21236&w=360" alt=""></div>
-                        <div class= "imagenes__divc"><img class="imagenes transition:0.6s" width="330" src="https://i.pinimg.com/originals/00/3c/7d/003c7dbe9a7afdf2669bc8a746f7b9a0.jpg" alt=""></div>
+                        <div class="accordion__imagenes"><img class="accordion__im" width="330" src="https://img.freepik.com/free-photo/portrait-happy-smiling-young-businessman-blue-suit-isolated-white-wall_231208-9205.jpg?w=360&t=st=1650476027~exp=1650476627~hmac=daab808c413f9dfa2bc9f898a243a2e91ad99185ed003ffaac0f6722c5ec62f2" alt=""></div>
+                        <div class="accordion__imagenes"><img class="accordion__im" width="330" src="https://img.freepik.com/free-photo/happy-business-man-wearing-grey-suit-standing-isolated-white-wall_231208-9211.jpg?w=360&t=st=1650475927~exp=1650476527~hmac=29590b8111508c31aebb2507a86d69e194bfb0c3c234836704a6c88a2048a5b2" alt=""></div>
+                        <div class="accordion__imagenes"><img class="accordion__im" width="330" src="https://img.freepik.com/free-photo/portrait-happy-smiling-young-businessman-brown-suit-isolated-white-wall_231208-9214.jpg?w=360&t=st=1650475876~exp=1650476476~hmac=658088d702884f8b4ad12cdfd79562f675bad3d68a24af6dbd855167693d3d6c" alt=""></div>
+                        <div class="accordion__imagenes"><img class="accordion__im" width="330" src="https://img.freepik.com/free-photo/full-body-portrait-businessman-suit-standing-isolated-white-wall_231208-9203.jpg?t=st=1650477958~exp=1650478558~hmac=dcb63693b7d8f006de49abe5cc686102b8e1461dbcdac824e7b6ae05855a1c92&w=360" alt=""></div>
+                        <div class="accordion__imagenes"><img class="accordion__im" width="330" src="https://img.freepik.com/free-photo/full-length-portrait-businessman-isolated-gray-wall_231208-9209.jpg?t=st=1650477958~exp=1650478558~hmac=feccece7be6cf7b73e4f3943a0b1637fac1fe5818cf604ff8912b01591d0e69c&w=360" alt=""></div>
+                        <div class="accordion__imagenes"><img class="accordion__im" width="330" src="https://img.freepik.com/free-photo/portrait-happy-smiling-young-businessman-brown-suit-isolated-white-wall_231208-9213.jpg?w=360&t=st=1650477966~exp=1650478566~hmac=aa9ff0b6f74c7d9d128c8eb999163648266b2b2ae3255b0f57129b3f5e704e13" alt=""></div>
+                        <div class="accordion__imagenes"><img class="accordion__im" width="330" src="https://img.freepik.com/foto-gratis/retrato-cuerpo-entero-empresario-blanco_155003-16199.jpg?w=360&t=st=1654194236~exp=1654194836~hmac=4396ecbf619be12b35c6acdcf17170d55cab2e60d3627fe890bd12396321961f" alt=""></div>
+                        <div class="accordion__imagenes"><img class="accordion__im" width="330" src="https://img.freepik.com/foto-gratis/concepto-negocio-retrato-cuerpo-entero-hombre-negocios-elegante-traje-elegante-caminando-sobre-fondo-blanco_1258-80289.jpg?t=st=1654188714~exp=1654189314~hmac=15550dfa18d069d979f6b59a751711d89c52ee645d7e2974a0893690e5d21236&w=360" alt=""></div>
+                        <div class="accordion__imagenes"><img class="accordion__im" width="330" src="https://i.pinimg.com/originals/00/3c/7d/003c7dbe9a7afdf2669bc8a746f7b9a0.jpg" alt=""></div>
                     </div>
                 </div>
             </div>
@@ -79,10 +79,12 @@
                 <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo"
                     data-bs-parent="#accordionExample">
                     <div class="accordion-body">
-                        <div class= "imagenes__divc"><img class="imagenes transition:0.6s" src="https://images.unsplash.com/photo-1594938298603-c8148c4dae35?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80" width="280" alt=""></div>
-                        <div class= "imagenes__divc"><img class="imagenes transition:0.6s" src="https://images.unsplash.com/photo-1594938328870-9623159c8c99?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80" width="280" alt=""></div>
-                        <div class= "imagenes__divc"><img class="imagenes transition:0.6s" src="https://images.unsplash.com/photo-1593032465175-481ac7f401a0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80" width="280" alt=""></div>
-                        <div class= "imagenes__divc"><img class="imagenes transition:0.6s" src="https://images.unsplash.com/photo-1592878904946-b3cd8ae243d0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=581&q=80" width="280" alt=""></div>
+                        <div class="accordion__imagenes"><img class="accordion__im" src="https://images.unsplash.com/photo-1594938298603-c8148c4dae35?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80" width="280" alt=""></div>
+                        <div class="accordion__imagenes"><img class="accordion__im" src="https://images.unsplash.com/photo-1594938328870-9623159c8c99?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80" width="280" alt=""></div>
+                        <div class="accordion__imagenes"><img class="accordion__im" src="https://images.unsplash.com/photo-1593032465175-481ac7f401a0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80" width="280" alt=""></div>
+                        <div class="accordion__imagenes"><img class="accordion__im" src="https://images.unsplash.com/photo-1592878904946-b3cd8ae243d0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=581&q=80" width="280" alt=""></div>
+                        <img src="" alt="">
+                        <img src="" alt="">
                     </div>
                 </div>
             </div>
@@ -97,12 +99,12 @@
                 <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree"
                     data-bs-parent="#accordionExample">
                     <div class="accordion-body">
-                        <div class= "imagenes__divc"><img class="imagenes transition:0.6s" src="https://realmodas.files.wordpress.com/2017/04/chaqueta-azul-pantalon-gris.jpg" width="330" alt=""></div>
-                        <div class= "imagenes__divc"><img class="imagenes transition:0.6s" src="https://realmodas.files.wordpress.com/2017/04/chaqueta-negra-pantalon-gris.jpg" width="330" alt=""></div>
-                        <div class= "imagenes__divc"><img class="imagenes transition:0.6s" src="https://realmodas.files.wordpress.com/2017/04/verde-azul-combinacion.jpg" width="330" alt=""></div>
-                        <div class= "imagenes__divc"><img class="imagenes transition:0.6s" src="https://realmodas.files.wordpress.com/2017/04/traje-pantalon-tonos-terrestres1.jpg" width="330" alt=""></div>
-                        <div class= "imagenes__divc"><img class="imagenes transition:0.6s" src="https://realmodas.files.wordpress.com/2017/04/chaqueta-lana-jeans.jpg" width="330" alt=""></div>
-                        <div class= "imagenes__divc"><img class="imagenes transition:0.6s" src="https://realmodas.files.wordpress.com/2017/04/traje-azul-pantalon-beige1.jpg" width="330" alt=""></div>
+                        <div class="accordion__imagenes"><img class="accordion__im" src="https://realmodas.files.wordpress.com/2017/04/chaqueta-azul-pantalon-gris.jpg" width="330" alt=""></div>
+                        <div class="accordion__imagenes"><img class="accordion__im" src="https://realmodas.files.wordpress.com/2017/04/chaqueta-negra-pantalon-gris.jpg" width="330" alt=""></div>
+                        <div class="accordion__imagenes"><img class="accordion__im" src="https://realmodas.files.wordpress.com/2017/04/verde-azul-combinacion.jpg" width="330" alt=""></div>
+                        <div class="accordion__imagenes"><img class="accordion__im" src="https://realmodas.files.wordpress.com/2017/04/traje-pantalon-tonos-terrestres1.jpg" width="330" alt=""></div>
+                        <div class="accordion__imagenes"><img class="accordion__im" src="https://realmodas.files.wordpress.com/2017/04/chaqueta-lana-jeans.jpg" width="330" alt=""></div>
+                        <div class="accordion__imagenes"><img class="accordion__im" src="https://realmodas.files.wordpress.com/2017/04/traje-azul-pantalon-beige1.jpg" width="330" alt=""></div>
                     </div>
                 </div>
             </div>
